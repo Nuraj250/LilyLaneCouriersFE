b4d60a84c1fdfabcd8dbc4faff3d97a1018fdc64
Nuraj@250 <nurajshaminda200@gmail.com>
2023-10-07 15:55:14 +0530

    home

--- src/app/component/landing/landing.component.ts
+++ src/app/component/landing/landing.component.ts
@@ -1,29 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
   styleUrls: ['./landing.component.scss']
 })
-export class LandingComponent implements OnInit  {
+export class LandingComponent {
 
-  slides: any[] = new Array(3).fill({id: -1, src: '', title: '', subtitle: ''});
-
-  constructor() {}
-
-  ngOnInit(): void {
-    this.slides[0] = {
-      src: './assets/img/angular.jpg',
-    };
-    this.slides[1] = {
-      src: './assets/img/react.jpg',
-    }
-    this.slides[2] = {
-      src: './assets/img/vue.jpg',
-    }
-  }
-
-  onItemChange($event: any): void {
-    console.log('Carousel onItemChange', $event);
-  }
 }
